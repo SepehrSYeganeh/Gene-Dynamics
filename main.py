20831bea50f4df731abce65724473e3f4c109d4e
Sepehr.S.Yeganeh <sesesapp@gmail.com>
2025-09-21 19:12:38 +0330

    update report

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from genedyn import *
 
 
 def main():
-    number_of_fixed_points = 512
-    generate_data(number_of_fixed_points)
+    number_of_stable_fixed_points = 512
+    generate_data(number_of_stable_fixed_points)
     cluster_stable_points()
     clustering_fixed_points()
     learning_dynamics_custom_sampling()
